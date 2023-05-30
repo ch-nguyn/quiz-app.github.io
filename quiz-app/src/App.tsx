a07abd3f284e4debf821b67ea603314f6290f76c
Chien <cn555444@gmail.com>
2023-05-31 01:21:02 +0700

    fix

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -2,16 +2,28 @@ import React, { useState } from "react";
 import "./App.css";
 import { StartGame } from "./components/StartGame";
 import { InGame } from "./components/InGame/InGame";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   let [startGame, setStartGame] = useState<boolean>(false);
   let handleStartGame = () => {
     setStartGame(true);
   };
+  const QuizApp = () => {
+    return (
+      <div className="main">
+        {startGame ? (
+          <InGame x={""} />
+        ) : (
+          <StartGame onClick={handleStartGame} />
+        )}
+      </div>
+    );
+  };
   return (
-    <div className="main">
-      {startGame ? <InGame x={""} /> : <StartGame onClick={handleStartGame} />}
-    </div>
+    <Routes>
+      <Route index element={<QuizApp />}></Route>
+    </Routes>
   );
 }
 
